78903ce970840a5e9cfd6d1a260f1ccce3a1bf35
nokk <nokk@MacBook-Air-nokk.local>
2026-03-13 15:07:37 +0300

    security conf

--- src/main/kotlin/ru/bonch/pivder/config/SecurityConfiguration.kt
+++ src/main/kotlin/ru/bonch/pivder/config/SecurityConfiguration.kt
@@ -8,17 +8,31 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import ru.bonch.pivder.app.jwt.JwtFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val jwtFilter: JwtFilter
+) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        TODO("end this")
         http
             .csrf { it.disable() }
             .sessionManagement { sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers(
+                        "/api/v1/auth/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui.html/**",
+                        "/swagger-ui/**"
+                    ).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
     }
 
